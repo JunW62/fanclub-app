79688cf31a7a6f8662e75c93445604ed8dbcc311
JunW62 <sivia1990@gmail.com>
2024-08-22 10:24:06 -0500

    add products

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,27 @@ const User = require("./models/user");
 const app = express();
 
 // Connect to the database
-connectDB()
-  .then(async () => {
-    console.log("MongoDB connected successfully");
+connectDB();
+// .then(async () => {
+//   console.log("MongoDB connected successfully");
 
-    // Test by creating a new user
-    const newUser = new User({
-      name: "Test User",
-      email: "testuser@example.com",
-      password: "testpassword",
-    });
+//   // Test by creating a new user
+//   const newUser = new User({
+//     name: "Test User",
+//     email: "testuser@example.com",
+//     password: "testpassword",
+//   });
 
-    try {
-      const savedUser = await newUser.save();
-      console.log("New user created:", savedUser);
-    } catch (error) {
-      console.error("Error creating user:", error.message);
-    }
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
-  });
+//   try {
+//     const savedUser = await newUser.save();
+//     console.log("New user created:", savedUser);
+//   } catch (error) {
+//     console.error("Error creating user:", error.message);
+//   }
+// })
+// .catch((err) => {
+//   console.error("Error connecting to MongoDB:", err.message);
+// });
 
 // Middleware to parse JSON bodies
 app.use(express.json());
