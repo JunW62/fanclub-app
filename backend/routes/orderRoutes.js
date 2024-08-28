993ec6511cd1d4fafad2c8c89dd4aaf00e0d5f52
JunW62 <sivia1990@gmail.com>
2024-08-28 10:26:46 -0500

    add auth

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,13 +3,13 @@ const orderController = require("../controllers/orderController");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-// Create a new order (requires auth)
+// Create a new order
 router.post("/orders", auth, orderController.createOrder);
 
-// Get all orders for a specific user (requires auth) - need the order id
+// Get all orders for a specific user  - need the order id
 router.get("/orders/user/:userId", auth, orderController.getOrdersByUser);
 
-// Get a specific order by ID (requires auth)
+// Get a specific order by ID
 router.get("/orders/:orderId", auth, orderController.getOrderById);
 
 module.exports = router;
